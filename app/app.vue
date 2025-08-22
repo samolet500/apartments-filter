aba916bd6c1d5293ede825912445c913f06315ed
Maksim Sakhuta <msakhuta@inetstudio.ru>
2025-08-22 12:47:49 +0300

    add fonts. add styles

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1 class="hello-page">Hello</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.hello-page {
+  color: $color-green;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
+  devtools: { enabled: false },
+  css: ['~/assets/styles/index.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/styles/variables.scss" as *;\n'
+        }
+      }
+    }
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
